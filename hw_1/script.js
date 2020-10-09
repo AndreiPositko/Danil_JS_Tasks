e6633e27d9cbb72be63f0d7797b18e097d96ef70
Andrei Positko <andrei.positko@chisw.com>
2020-10-09 16:40:11 +0300

    first task

---
+++ hw_1/script.js
@@ -0,0 +1,8 @@
+/*1 Cоздать переменные со всеми возможными типами данных.*/
+let number = 777;
+const name = 'Petro';
+let isTrue = false;
+let id = Symbol('id');
+let bigint = 525448744684616167416161516845116845n;
+let age = null;
+let height = undefined;
