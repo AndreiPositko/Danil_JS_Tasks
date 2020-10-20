8e0f9c72677123bceb9340879005ac65aeee1ea6
Andrei Positko <andrei.positko@chisw.com>
2020-10-20 17:38:25 +0300

    hw-1/2

--- hw_1/script.js
+++ hw_1/script.js
@@ -6,3 +6,17 @@ let id = Symbol('id');
 let bigint = 525448744684616167416161516845116845n;
 let age = null;
 let height = undefined;
+
+/*2
+Создать обьект который будет описывать любой предмет или сущность. Указать минимум 10 свойств описуемого обьекта. Свойства должны охватывать все типы данных. Если это авто, то обьект может содержать свойста: name, price, manufacture and etc.*/
+
+let person = {
+    name: 'Vasya',
+    age: 32,
+    height: 190,
+    weight: 95,
+    eyesColor: 'blue',
+    hairColor: 'green',
+    is
+
+}
